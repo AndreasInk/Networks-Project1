5bb2bf9b3bd93aa915a693ac3cff778c03971f78
Andreas Ink <andreasink17@gmail.com>
2023-11-27 02:23:13 -0500

    make client multithreaded

--- IterativeSocketClient-Python/app.py
+++ IterativeSocketClient-Python/app.py
@@ -1,6 +1,8 @@
 import requests
 import datetime
 from statistics import mean
+import threading
+
 # Constant strings we may use for errors or other messaging
 requestErrorDescription = "Request failed, please try again"
 commands = ["d = date time", "u = up time", "m = memory usage", "n = network connections", "p = running processes", "cu = current users"]
@@ -34,23 +36,29 @@ def fetch(endpoint: str):
     return requestErrorDescription
 
 # For basic endpoints that just return a number
-def getBasicEndpoint(endpoint: str):
+def getBasicEndpoint(endpoint: str, iterationStartDate: float):
     response = fetch(endpoint)
+    
     if response == requestErrorDescription: 
         print(requestErrorDescription) 
         return 
     try:
         json = response.json()
+        print(json[endpoint])
+        averageTimes.append(datetime.datetime.now().timestamp() - iterationStartDate)
         return json[endpoint]
     except:
         print(requestErrorDescription)
         return
 
 # For complex endpoints that return a few componets
-def getComplexEndpoint(endpoint: str):
-    dateTimeResponse = fetch(endpoint)
-    if dateTimeResponse == requestErrorDescription: return 
-    json = dateTimeResponse.json()
+def getComplexEndpoint(endpoint: str, columnNames: list, iterationStartDate: float):
+    
+    response = fetch(endpoint)
+    if response == requestErrorDescription: return 
+    json = response.json()
+    averageTimes.append(datetime.datetime.now().timestamp() - iterationStartDate)
+    printJSON(columnNames, json)
     return json
 
 # Given columns, this method prints the column and value
@@ -62,34 +70,44 @@ def printJSON(columns: [str], json):
             except:
                 print("Column not found")
 
+threads = []
+
+def startThread(type: str, columnNames: list = None):
+    thread = threading.Thread()
+    iterationStartDate = datetime.datetime.now().timestamp()
+    if columnNames == None:
+        thread = threading.Thread(target=getBasicEndpoint, args=(type,iterationStartDate,))
+    else:
+         thread = threading.Thread(target=getComplexEndpoint, args=(type,columnNames,iterationStartDate,))
+    thread.start()
+    threads.append(thread)
 # Start listening for new input
 while inputKey != "" or inputKey != "q":
+    
     for iteration in range(0, iterations):
-        iterationStartDate = datetime.datetime.now().timestamp()
+        
         if inputKey == "d":
-            print(getBasicEndpoint("dateTime"))
+            startThread("dateTime")
              
         if inputKey == "u":
-            print(getBasicEndpoint("upTime"))
+            startThread("upTime")
 
         if inputKey == "m":
-            print(getBasicEndpoint("memoryUsage"))
+            startThread("memoryUsage")
 
         if inputKey == "n":
-            networkConnections = getComplexEndpoint("networkConnections")
-            printJSON(["proto", "receiveQueue", "sendQueue", "localAddress", "foreignAddress"], networkConnections)
+            startThread("networkConnections", columnNames=["proto", "receiveQueue", "sendQueue", "localAddress", "foreignAddress"])
         
         if inputKey == "p":
-            runningProcesses = getComplexEndpoint("runningProcesses")
-            print(runningProcesses)
-            printJSON(["user", "pid"], runningProcesses)
+            startThread("runningProcesses", ["user", "pid"])
         
         if inputKey == "cu":
-            currentUsers = getComplexEndpoint("currentUsers")
-            printJSON(["name", "host"], currentUsers)
-
-        averageTimes.append(datetime.datetime.now().timestamp() - iterationStartDate)
-
+            startThread("currentUsers", ["name", "host"])
+    
+    # Wait for all threads to complete
+    for thread in threads:
+        thread.join()
+        
     inputKey = input(inputInstructions)
     iterations = int(input(iterationInstructions))
 
